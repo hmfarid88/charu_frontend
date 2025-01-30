09b244469ae1cd69a960c9c6d95d0b285a692fa2
hmfarid88 <hmfariduddin189@gmail.com>
2025-01-30 06:52:56 +0600

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -11,67 +11,10 @@ const nextConfig = {
     },
     experimental: {
         serverActions: {
-          allowedOrigins: ['http://localhost:8080/bake_boss_backend'],
+          allowedOrigins: ['http://localhost:8080/charu_backend'],
         },
       },
 
 };
 
 
-
-// import withMDX from '@next/mdx';
-// import dotenv from 'dotenv';
-
-// dotenv.config({ path: `./.env.${process.env.ENVIRONMENT}` });
-
-// const mdxConfig = withMDX({
-//   extension: /\.(md|mdx)$/,
-// });
-
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'lh3.googleusercontent.com',
-//       },
-//     ],
-//   },
-//   env: {
-//     SESSION_SECRET: process.env.SESSION_SECRET,
-//     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-//   },
-//   webpack(config, options) {
-//     return config;
-//   },
-
-// };
-
-// export default mdxConfig(nextConfig);
-// import dotenv from 'dotenv';
-// dotenv.config({
-//     path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
-// });
-
-// const allowedOrigins =
-//     process.env.NODE_ENV === 'production'
-//         ? ['http://64.227.161.25/bake_boss_backend']
-//         : ['http://localhost:8080'];
-
-// const nextConfig = {
-//     images: {
-//         remotePatterns: [
-//             {
-//                 protocol: "https",
-//                 hostname: "lh3.googleusercontent.com"
-//             }
-//         ]
-//     },
-//     experimental: {
-//         serverActions: {
-//             allowedOrigins
-//         }
-//     }
-// };
-
-// export default nextConfig;
