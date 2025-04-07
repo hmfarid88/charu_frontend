15fb96d1951a4abf663f09e6dc42f8d0af2a1a77
hmfarid88 <hmfariduddin189@gmail.com>
2025-04-08 00:37:38 +0600

    updated

--- src/app/(dashboard-item)/dashboard/page.tsx
+++ src/app/(dashboard-item)/dashboard/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import Barchart from "@/app/components/Barchart";
 import HomeSummary from "@/app/components/HomeSummary";
 
--- src/app/components/HomeSummary.tsx
+++ src/app/components/HomeSummary.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useEffect, useState } from 'react'
 import { useAppSelector } from '../store';
 import { HiCurrencyBangladeshi } from "react-icons/hi";
