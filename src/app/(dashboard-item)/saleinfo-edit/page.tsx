f86cfafd4a10cb4649732a38584ca754ca4014db
hmfarid88 <hmfariduddin189@gmail.com>
2025-09-15 12:02:24 +0600

    updated

--- src/app/(dashboard-item)/saleinfo-edit/page.tsx
+++ src/app/(dashboard-item)/saleinfo-edit/page.tsx
@@ -80,7 +80,29 @@ const Page = () => {
 
         }
     };
+    const handleDeleteSubmit = async (e: any) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`${apiBaseUrl}/api/deleteProductSale/${productId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
 
+            });
+
+            if (!response.ok) {
+                // const error = await response.json();
+                toast.error("Sorry, item is not deleted!");
+            } else {
+                toast.success("Item deleted successfully.");
+
+            }
+
+        } catch (error: any) {
+            toast.error(error.message)
+        }
+    }
 
     const [itemOption, setItemOption] = useState([]);
     useEffect(() => {
@@ -192,7 +214,21 @@ const Page = () => {
 
                 </label>
             </div>
+            <div className="flex items-center justify-center p-2">
+                <label className="form-control w-full max-w-xs pt-5">
+                    <button className="btn btn-error"
+                        onClick={(e) => {
+                            if (window.confirm("Are you sure you want to delete this item?")) {
+                                handleDeleteSubmit(e);
+                            }
+                        }}
 
+                    >
+                        DELETE THIS ITEM
+                    </button>
+
+                </label>
+            </div>
         </div>
     )
 }
